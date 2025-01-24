0a9b36510ef704318d08ee7ff49c02bd63398338
T1000 <T1000@DESKTOP-B29GVN0>
2025-01-24 14:00:05 -0500

    First Commit after Clone

--- src/cardgame/Card.java
+++ src/cardgame/Card.java
@@ -1,14 +1,11 @@
 package cardgame;
-
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
  *
  * @author fagun
+ * @modifier dong nguyen
+ * @student id 991756472
  */
+
 public class Card {
      public enum Suit
    {
